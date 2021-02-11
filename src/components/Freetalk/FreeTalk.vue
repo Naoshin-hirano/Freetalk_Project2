24452fa37bb08d015712e7886931e09016325a33
Naoshin-hirano <hnaoya1120@gmail.com>
2021-02-11 16:55:48 +0900

    Freetalk.vueのリロード時のエラー解消

--- src/components/Freetalk/FreeTalk.vue
+++ src/components/Freetalk/FreeTalk.vue
@@ -6,13 +6,14 @@
                     <v-card-title>
                         <div class="ml-10">
                             <v-btn
+                            v-if="freetalk"
                             height="70px"
                             fab accent
-                            :to="'/profileforothers/' + creater.id">
+                            :to="'/profileforothers/' + freetalk.createrId">
                                 <v-avatar size="85" >
                                     <img
-                                    v-if="creater.photoURL"
-                                    :src="creater.photoURL"
+                                    v-if="freetalk.photoURL"
+                                    :src="freetalk.photoURL"
                                     alt="John">
                                     <img
                                     v-else
@@ -21,62 +22,65 @@
                                 </v-avatar>
                             </v-btn>
                         </div>
-                        <h3 class="ml-15">{{ freetalk.title }}</h3>
+                        <h3 class="ml-15">{{ freetalk ? freetalk.title : "" }}</h3>
                         <v-spacer></v-spacer>
                         <v-spacer></v-spacer>
                         <v-spacer></v-spacer>
                         <v-layout mb-7>
                             <v-flex >
-                                <likes-btn :freetalk="freetalk"></likes-btn>
+                                <likes-btn v-if="freetalk" :freetalk="freetalk"></likes-btn>
                             </v-flex>
                             <v-flex>
-                                 <delete-free-talk :freetalk="freetalk" width="500"></delete-free-talk>
+                                 <delete-free-talk v-if="freetalk" :freetalk="freetalk" width="500"></delete-free-talk>
                             </v-flex>
                             <v-flex>
-                                <edit-free-talk :freetalk="freetalk"></edit-free-talk>
+                                <edit-free-talk v-if="freetalk" :freetalk="freetalk"></edit-free-talk>
                             </v-flex>
                         </v-layout>
                     </v-card-title>
                     <v-img
+                     v-if="freetalk"
                      :src="freetalk.imageUrl"
                      height="500"
                     ></v-img>
                     <v-card-text>
-                        <p class="primary--text mb-0">{{ freetalk.date | date }} - {{ freetalk.location}}</p>
-                        <p class="mt-0">Language: {{ freetalk.language }}</p>
+                        <p v-if="freetalk" class="primary--text mb-0">{{ freetalk.date | date }} - {{ freetalk.location}}</p>
+                        <p v-if="freetalk" class="mt-0">Language: {{ freetalk.language }}</p>
                         <v-chip-group column class="ml-3">
                            <div class="mr-10">
                                <edit-date
+                                v-if="freetalk"
                                 :freetalk="freetalk"
                                 ></edit-date>
                             </div>
                             <div class="ml-5">
                                 <edit-time
+                                v-if="freetalk"
                                 :freetalk="freetalk"
                                 ></edit-time>
                             </div>
                         </v-chip-group>
-                        <div class="mt-5 mb-5">{{ freetalk.description }}</div>
+                        <div v-if="freetalk" class="mt-5 mb-5">{{ freetalk.description }}</div>
                     </v-card-text>
                     <v-layout>
                         <v-flex xs3 sm3 md3>
                             <h3 class="ml-5">出席者Pick Up</h3>
                         </v-flex>
-                        <v-flex xs3 sm3 md3>
+                        <v-flex xs3 sm3 md3 v-show="filterAttendance.length">
                              <v-btn
+                            v-if="freetalk"
                             :to="'/freetalks/' + freetalk.id + '/attendance/'"
                             text
-                            class="blue--text"
-                            v-show="filterAttendance.length">
-                            <v-icon large color="blue">mdi-chevron-left</v-icon>
-                            <h3>全出席者({{ filterAttendance.length }})を見る</h3>
+                            class="blue--text">
+                               <v-icon large color="blue">mdi-chevron-left</v-icon>
+                               <h3 v-if="filterAttendance">全出席者({{ filterAttendance.length }})を見る</h3>
                             </v-btn>
                         </v-flex>
                     </v-layout>
                     <v-card-actions>
                         <show-attendees></show-attendees>
                         <v-spacer></v-spacer>
-                        <register-dialog class="mr-2 mt-10" :freetalkId="freetalk.id"></register-dialog>
+                        <register-dialog class="mr-2 mt-10" v-if="freetalk" :freetalkId="freetalk.id"></register-dialog>
                     </v-card-actions>
                 </v-card>
             </v-flex>
@@ -85,33 +89,12 @@
 </template>
 
 <script>
-import db from "firebase"
 export default {
    props: ["id"],
-   data () {
-      return {
-        creater: {
-          id: "",
-          photoURL: ""
-        }
-      }
-    },
-   created(){
-        db.database().ref("/users/" + this.createrId).once("value").then(data =>{
-            this.creater.id = data.val().id,
-            this.creater.photoURL = data.val().photoURL
-        })
-    },
    computed: {
      freetalk(){
-      return this.id ? this.$store.getters.loadedFreeTalk(this.id) : ""
+      return this.id ? this.$store.getters.loadedFreeTalk(this.id) : null
      },
-     user(){
-      return this.$store.getters.user
-     },
-    createrId(){
-      return this.freetalk ? this.freetalk.createrId : ""
-    },
     filterAttendance(){
         return this.$store.getters.attendance.filter((data) =>{
           return data.freetalkId === this.$route.params.id
